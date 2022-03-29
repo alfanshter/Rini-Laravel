c72c1e566d6c055ecc1fcd8e39a17bc2a23a18a6
alfanshter <alfanno8@gmail.com>
2022-03-29 16:13:15 +0700

    Nilai Admin

--- app/Http/Controllers/NilaiController.php
+++ app/Http/Controllers/NilaiController.php
@@ -25,6 +25,14 @@ class NilaiController extends Controller
 
         }
 
+        if (auth()->user()->role ==0) {
+            $ekskul = InformasiEkskul::join('data_ekskuls','data_ekskuls.kode', '=','informasi_ekskuls.kode_ekskul')
+            ->get();
+            return view('nilai.daftarnilai',['ekskul' =>$ekskul]);    
+
+        }
+
+
         else if (auth()->user()->role ==1) {
             $ekskul = DB::table('ekskuls')
                 ->join('data_ekskuls','data_ekskuls.kode','=','ekskuls.kode_ekskul')
@@ -71,11 +79,82 @@ class NilaiController extends Controller
     
         }
 
+        else if (auth()->user()->role ==0){
+            $validatedData = $request->validate([
+                'nama_ekskul' => 'required|max:255',
+                'id_siswa' => ['required'],
+                'nilai' => ['required'],
+                'tahun_ajaran' => ['required'],
+                'semester' => ['required']
+            ]);
+    
+            //getid ekskul
+            $getekskul = DB::table('data_ekskuls')
+                            ->where('nama', $request->nama_ekskul)
+                            ->first();
+    
+            //Nama Siswa
+            $siswa = DB::table('users')
+            ->where('nim', $request->id_siswa)
+            ->first();
+
+            //Nama Pelatih
+            $namapelatih = DB::table('data_ekskuls')
+            ->select(['ekskuls.kode_pelatih as id_pelatih','users.name as nama_pelatih'])
+            ->join('ekskuls','ekskuls.kode_ekskul','=','data_ekskuls.kode')
+            ->join('users','users.nim','=','ekskuls.kode_pelatih')
+            ->where('data_ekskuls.nama',$request->nama_ekskul)
+            ->first();
+
+            $validatedData['nama_pelatih'] = $namapelatih->nama_pelatih;
+            $validatedData['id_pelatih'] = $namapelatih->id_pelatih;
+            $validatedData['id_ekskul'] = $getekskul->kode;
+            $validatedData['nama_siswa'] = $siswa->name;
+    
+            Nilai::create($validatedData);
+    
+            return redirect("/data_nilai/$request->nama_ekskul")->with('success','Nilai berhasil di input');
+    
+
+        }
+
     }
 
     public function daftar_nilai($nama_ekskul)
     {
-        if (auth()->user()->role ==2) {
+        if (auth()->user()->role ==0) {
+             //idpelatih
+             $namapelatih = DB::table('data_ekskuls')
+             ->select(['ekskuls.kode_pelatih as id_pelatih','users.name as nama_pelatih'])
+             ->join('ekskuls','ekskuls.kode_ekskul','=','data_ekskuls.kode')
+             ->join('users','users.nim','=','ekskuls.kode_pelatih')
+             ->where('data_ekskuls.nama',$nama_ekskul)
+             ->first();
+            
+            //nama siswa
+            $nama_siswa = Ekskul::join('users','users.nim', '=' ,'ekskuls.nim_siswa')
+            ->join('data_ekskuls','data_ekskuls.kode', '=', 'ekskuls.kode_ekskul')
+            ->where('data_ekskuls.nama',$nama_ekskul)
+            ->where('data_ekskuls.nama', $nama_ekskul)
+            ->where('is_status', 2)
+            ->get(['users.name','users.nim']);
+            
+
+
+            $nilai = DB::table('nilais')
+                    ->where('id_pelatih',$namapelatih->id_pelatih)
+                    ->where('nama_ekskul', $nama_ekskul)
+                    ->get();
+
+                
+            return view('nilai.nilai',
+                ['nilai' =>$nilai,
+                'nama_siswa' => $nama_siswa,
+                'namapelatih' => $namapelatih,
+                'nama_ekskul' => $nama_ekskul
+                ]);   
+        }
+        else if (auth()->user()->role ==2) {
             //nama siswa
             $nama_siswa = Ekskul::join('users','users.nim', '=' ,'ekskuls.nim_siswa')
             ->join('data_ekskuls','data_ekskuls.kode', '=', 'ekskuls.kode_ekskul')
@@ -94,7 +173,8 @@ class NilaiController extends Controller
                 'nama_siswa' => $nama_siswa,
                 'nama_ekskul' => $nama_ekskul
                 ]);   
-        } else if (auth()->user()->role ==1){
+        }
+         else if (auth()->user()->role ==1){
             $nilai = DB::table('nilais')
             ->where('id_siswa', auth()->user()->nim)
             ->where('nama_ekskul', $nama_ekskul)
@@ -108,26 +188,20 @@ class NilaiController extends Controller
 
     public function destroy($id)
     {
-        if (auth()->user()->role ==2) {
             Nilai::destroy($id);
             return redirect('/nilai')->with('success', 'Nilai berhasil di hapus ');            
-        }
     }
 
     public function edit($id)
     {
-        if (auth()->user()->role ==2) {
             $nilai = Nilai::where('id',$id)->first();;
             return view('nilai.edit_nilai',['nilai'=>$nilai]);
-        }
 
     }
 
     public function update(Request $request)
     {
-        if (auth()->user()->role ==2) {    
            
-
             $update = DB::table('nilais')->
                 where('id',$request->id)->update([
                 'tahun_ajaran' => $request->tahun_ajaran,
@@ -135,15 +209,11 @@ class NilaiController extends Controller
             ]);
     
             return redirect("/nilai/$request->id")->with('success','Nilai berhasil di Edit');
-    
-    
-        }
 
     }
 
     public function cetakpdf_nilai($nama_ekskul)
     {
-        if (auth()->user()->role == 2) {
 
             $nilai = Nilai::where('nama_ekskul',$nama_ekskul)
                     ->select(['nilais.*','users.kelas'])
@@ -154,7 +224,6 @@ class NilaiController extends Controller
                 'nilai'=>$nilai        
             ]);
             return $pdf->stream();
-        }
     }
 
 
--- resources/views/nilai/nilai.blade.php
+++ resources/views/nilai/nilai.blade.php
@@ -7,47 +7,70 @@
                     <!-- Page Heading -->
                     <h1 class="h3 mb-2 text-gray-800">Nilai</h1>
                     @if (auth()->user()->role ==0)
-                        <button class="btn btn-primary"  data-toggle="modal" data-target="#tambahpengumuman">Tambah Nilai</button>                        
-                            <!-- Pengumuman Modal-->
-                        <div class="modal fade" id="tambahpengumuman" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-                            aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">Tambah Nilai?</h5>
-                                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">×</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <form action="/nilai" method="POST">
-                                            @csrf
-                                            <div class="mb-3">
-                                            <label for="recipient-name" class="col-form-label">Nama Siswa:</label>
-                                            <input type="text" class="form-control" id="nama_siswa" name="nama_siswa" value="{{old('nama_siswa')}}">
-                                            </div>
-                                            <div class="mb-3">
-                                            <label for="recipient-name" class="col-form-label">Nama Ekskul:</label>
-                                            <input type="text" class="form-control" id="nama_ekskul" name="nama_ekskul" value="{{old('nama_ekskul')}}">
-                                            </div>
-
-                                            <div class="mb-3">
-                                                <label for="recipient-name" class="col-form-label">Nilai</label>
-                                                <input type="number" class="form-control" id="nilai" name="nilai" value="{{old('nilai')}}">
-                                            </div>
-                                            <div class="modal-footer">
-                                            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                                            <button type="submit" class="btn btn-primary">Tambah</button>
-        
-                                        </div>
-                                        </form>
-                                        </div>
-                                    
-
+                    <button class="btn btn-primary"  data-toggle="modal" data-target="#tambahpengumuman">Tambah Nilai</button>                        
+                        <!-- Pengumuman Modal-->
+                    <div class="modal fade" id="tambahpengumuman" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                        aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Tambah Nilai?</h5>
+                                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">×</span>
+                                    </button>
                                 </div>
+                                <div class="modal-body">
+                                    <form action="/nilai" method="POST">
+                                        @csrf
+                                        <input type="hidden" name="nama_ekskul" id="nama_ekskul" value="{{$nama_ekskul}}">
+                                        <div class="mb-3">
+                                            <label for="recipient-name" class="col-form-label">Nama Siswa:</label>
+                                            <select class="form-control" aria-label="Default select example" name="id_siswa" id="id_siswa">
+                                                @foreach ($nama_siswa as $data)
+                                                <option value="{{$data->nim}}">
+                                                    {{$data->name}}
+                                                </option>                                                
+                                                @endforeach
+                                            </select>
+                                        </div>
+
+                                        <div class="mb-3">
+                                            <label for="recipient-name" class="col-form-label">Semester:</label>
+                                            <select class="form-control" aria-label="Default select example" name="semester" id="semester">
+                                                <option value="ganjil">
+                                                    GANJIL
+                                                </option>                                                
+                                                <option value="genap">
+                                                    GENAP
+                                                </option>                                                
+
+                                            </select>
+                                        </div>
+
+                                        <div class="mb-3">
+                                            <label for="recipient-name" class="col-form-label">Tahun Ajaran:</label>
+                                            <input type="text" class="form-control" id="tahun_ajaran" name="tahun_ajaran" value="{{old('tahun_ajaran')}}">
+                                            <p>Cara penulisan : 2022-2021</p>
+                                        </div>
+
+                                        <div class="mb-3">
+                                            <label for="recipient-name" class="col-form-label">Nilai</label>
+                                            <input type="number" class="form-control" id="nilai" name="nilai" value="{{old('nilai')}}">
+                                        </div>
+                                        <div class="modal-footer">
+                                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                                        <button type="submit" class="btn btn-primary">Tambah</button>
+    
+                                    </div>
+                                    </form>
+                                    </div>
+                                
+
                             </div>
                         </div>
-                    @endif
+                    </div>
+                @endif
+
 
                     @if (auth()->user()->role ==2)
                         <button class="btn btn-primary"  data-toggle="modal" data-target="#tambahpengumuman">Tambah Nilai</button>                        
@@ -112,7 +135,7 @@
                                 </div>
                             </div>
                         </div>
-                        @endif
+                    @endif
 
                     @if (session()->has('success'))
                         <div class="alert alert-success mt-2" role="alert">
@@ -138,6 +161,11 @@
                     </div>                        
                     @enderror
                     
+                    @error('id_siswa')
+                    <div class="alert alert-danger mt-2" role="alert">
+                        {{$message}}  
+                    </div>                        
+                    @enderror
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4 mt-4">
                         <div class="card-header py-3 d-flex">
@@ -160,7 +188,7 @@
                                             <th>Predikat</th>
                                             <th>Tahun Ajaran</th>
                                             <th>Semester</th>
-                                            @if (auth()->user()->role ==2)
+                                            @if (auth()->user()->role ==2 || auth()->user()->role ==0)
                                             <th>Action</th>                                                
                                             @endif
                                         </tr>
@@ -187,13 +215,13 @@
                                             </td>
                                             <td>{{$data->tahun_ajaran}}</td>
                                             <td>{{$data->semester}}</td>
-                                            @if (auth()->user()->role ==2)
+                                            @if (auth()->user()->role ==2 || auth()->user()->role ==0)
                                             <td class="align-middle text-center">
                                                 <div class="d-flex justify-content-sm-center mt-2">
                                                 
                                                     <a href="/nilai/{{$data->id}}" class="btn btn-warning">Edit</a>
 
-                                                    <form action="/nilai/{{$data->id}}" method="post">
+                                                    <form action="/hapusnilai/{{$data->id}}" method="post">
                                                         @method('delete')
                                                         @csrf
                                                         <button class="btn btn-danger ml-2" onclick="return confirm('Apakah anda menyetujui ?')">Hapus</button>
